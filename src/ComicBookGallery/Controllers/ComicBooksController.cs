74a5d9a0d766f8c0292a95aaf07bc8ba3befe0ea
EtherealBoB <bobballew@gmail.com>
2017-11-07 12:43:03 -0500

    Added ComicBooks Detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,11 +10,14 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return new RedirectResult("/");
-            }
-            return Content("Hello from the CBC!");
+            return View();
+
+            //this is just code I wanted to keep for reference
+            //if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            //{
+            //    return new RedirectResult("/");
+            //}
+            //return Content("Hello from the CBC!");
             //this code operates the same as the line above.
             //return new ContentResult()
             //{
