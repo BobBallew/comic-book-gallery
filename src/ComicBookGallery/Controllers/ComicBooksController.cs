e435d13f8444a05f9413e1c3e3af8cc33e41df03
EtherealBoB <bobballew@gmail.com>
2017-11-06 22:36:34 -0500

    Returns ActionResults in Deatil action method.

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,18 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return new RedirectResult("/");
+            }
+            return Content("Hello from the CBC!");
+            //this code operates the same as the line above.
+            //return new ContentResult()
+            //{
+            //   Content = "Hello fromthe CBC!"
+            //};
+        }
     }
 }
